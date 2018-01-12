6b33ced3fc5178cc12f3343bd8a59b5ce6936fc0
lochuynh2613 <loc.h2613@gmail.com>
2018-01-12 15:15:48 +0700

    Photogram

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+	validates :user_id, presence: true
+
+	has_attached_file :image, presence: true
+	has_attached_file :image, styles:{:med => "640x640"}
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	
+	has_many :comments, dependent: :destroy
+	belongs_to :user
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  resources :posts do
+  	resources :comments
+  end
+
+  resources :posts
+end
